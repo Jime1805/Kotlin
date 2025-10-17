b9427898ca028fd415cfbcccf715aa3680f08c4a
Jime1805 <18ejimen@gmail.com>
2025-10-17 19:56:12 +0200

    primers widgets

---
+++ app/src/main/java/com/example/actv4_imc/ImcCalculator/ImcCalculatorActivity.kt
@@ -0,0 +1,15 @@
+package com.example.actv4_imc.ImcCalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.actv4_imc.R
+
+class ImcCalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_imc_calculator)
+    }
+}
--- app/src/main/java/com/example/actv4_imc/MainActivity.kt
+++ app/src/main/java/com/example/actv4_imc/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.actv4_imc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.actv4_imc.ImcCalculator.ImcCalculatorActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val btnSaludaApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        btnIMCApp.setOnClickListener { navigateToIMCApp() }
+    }
+    private fun navigateToIMCApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
     }
 }
