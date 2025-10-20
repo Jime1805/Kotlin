46f8ce00c62fd82b508e6a8193d4b9f1437b9b2f
Jime1805 <18ejimen@gmail.com>
2025-10-20 19:17:24 +0200

    altura fet pes començat

--- app/src/main/java/com/example/actv4_imc/ImcCalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/actv4_imc/ImcCalculator/ImcCalculatorActivity.kt
@@ -1,11 +1,16 @@
 package com.example.actv4_imc.ImcCalculator
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.actv4_imc.R
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class ImcCalculatorActivity : AppCompatActivity() {
 
@@ -15,31 +20,64 @@ class ImcCalculatorActivity : AppCompatActivity() {
     private lateinit var viewMale: CardView
     private lateinit var viewFemale: CardView
 
+    private lateinit var tvHeight: TextView
+    private lateinit var rsHeight: RangeSlider
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
         initComponent()
         initListeners()
+        initUI()
     }
 
     private fun initComponent(){
         viewMale = findViewById(R.id.viewMale)
         viewFemale = findViewById(R.id.viewFemale)
-    }
-    private fun initListeners() {
-        viewMale.setOnClickListener {setGenderColor()}
-        viewFemale.setOnClickListener {setGenderColor()}
+        tvHeight = findViewById(R.id.tvHeight)
+        rsHeight = findViewById(R.id.rsHeight)
 
     }
-    private fun setGenderColor(){
-        val maleColor = if (isMaleSelected) {
-            R.color.background_component_selected}
-        else {
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+
+        rsHeight.addOnChangeListener {_, value, _ ->
+            val df = DecimalFormat("#.##")
+            val result = df.format(value)
+            tvHeight.text = "$result cm"
+        }
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+
+        val colorReference = if (isSelectedComponent) {
+            R.color.background_component_selected
+        }else {
             R.color.background_component
         }
 
-        viewMale.setCardBackgroundColor(maleColor)
-        viewFemale.setCardBackgroundColor()
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+
     }
 }
