7ff6e3a841878532fab7655189dcd4374469b859
Jime1805 <18ejimen@gmail.com>
2025-10-20 09:01:47 +0200

    home y dona en proces

--- app/src/main/java/com/example/actv4_imc/ImcCalculator/ImcCalculatorActivity.kt
+++ app/src/main/java/com/example/actv4_imc/ImcCalculator/ImcCalculatorActivity.kt
@@ -8,8 +8,38 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.actv4_imc.R
 
 class ImcCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc_calculator)
+        initComponent()
+        initListeners()
+    }
+
+    private fun initComponent(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+    private fun initListeners() {
+        viewMale.setOnClickListener {setGenderColor()}
+        viewFemale.setOnClickListener {setGenderColor()}
+
+    }
+    private fun setGenderColor(){
+        val maleColor = if (isMaleSelected) {
+            R.color.background_component_selected}
+        else {
+            R.color.background_component
+        }
+
+        viewMale.setCardBackgroundColor(maleColor)
+        viewFemale.setCardBackgroundColor()
     }
 }
