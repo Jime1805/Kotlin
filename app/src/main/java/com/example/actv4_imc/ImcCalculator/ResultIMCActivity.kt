202666c31572e0206e11223c983d2f4e34d5de48
Jime1805 <18ejimen@gmail.com>
2025-10-22 17:49:52 +0200

    actv2-IMC acabada

--- app/src/main/java/com/example/actv4_imc/ImcCalculator/ResultIMCActivity.kt
+++ app/src/main/java/com/example/actv4_imc/ImcCalculator/ResultIMCActivity.kt
@@ -1,9 +1,11 @@
 package com.example.actv4_imc.ImcCalculator
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.actv4_imc.ImcCalculator.ImcCalculatorActivity.Companion.IMC_KEY
@@ -14,6 +16,7 @@ class ResultIMCActivity : AppCompatActivity() {
     private lateinit var tvResult:TextView
     private lateinit var tvIMC:TextView
     private lateinit var tvDescription: TextView
+    private lateinit var btnRecalculate: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,11 @@ class ResultIMCActivity : AppCompatActivity() {
         val result: Double = intent.extras?.getDouble(IMC_KEY) ?: -1.0
         initComponents()
         initUi(result)
+        initListeners()
+    }
+
+    private fun initListeners(){
+        btnRecalculate.setOnClickListener { onBackPressed() }
     }
 
     private fun initUi(result: Double) {
@@ -28,18 +36,22 @@ class ResultIMCActivity : AppCompatActivity() {
         when(result){
             in 0.00..18.50 -> {
                 tvResult.text = getString(R.string.title_bajo_peso)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.peso_bajo))
                 tvDescription.text = getString(R.string.description_bajo_peso)
             }
             in 18.51..24.99 -> {
                 tvResult.text = getString(R.string.title_peso_normal)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.peso_normal))
                 tvDescription.text = getString(R.string.description_peso_normal)
             }
             in 25.00..29.99 -> {
                 tvResult.text = getString(R.string.title_sobrepeso)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.peso_sobrepeo))
                 tvDescription.text = getString(R.string.description_sobrepeso)
             }
             in 30.00..99.00 -> {
                 tvResult.text = getString(R.string.title_obesidad)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesidad))
                 tvDescription.text = getString(R.string.description_obesidad)
             }
             else -> {
@@ -53,6 +65,7 @@ class ResultIMCActivity : AppCompatActivity() {
         tvIMC = findViewById(R.id.tvIMC)
         tvResult = findViewById(R.id.tvResult)
         tvDescription = findViewById(R.id.tvDescription)
+        btnRecalculate = findViewById(R.id.btnRecalculate)
     }
 
 }
